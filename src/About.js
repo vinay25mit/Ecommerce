e1d9e1e6f9922f10fbe2eb6121928c1b923dca67
vinay25mit <vinay25mit@gmail.com>
2026-01-05 09:40:59 +0530

    initial ui

--- src/About.js
+++ src/About.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import HeroSection from "./components/HeroSection";
+// import { useProductContext } from "./context/productcontex";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  // const { myName } = useProductContext();
 
-export default About
+  const data = {
+    name: "Thapa Ecommerce",
+  };
+
+  return (
+    <>
+      
+      <HeroSection myData={data} />
+    </>
+  );
+};
+
+export default About;
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import styled from 'styled-components'
-const Home = () => {
-  return (
-    <Wrapper className='test'>Home</Wrapper>
-  )
-};
-const Wrapper=styled.section`
-background-color:${({theme})=>theme.colors.bg};
- width:20rem;
- height:20rem
-`;
+// import FeatureProduct from "./components/FeatureProduct";
+import HeroSection from "./components/HeroSection";
+import Services from "./components/Services";
+import Trusted from "./components/Trusted";
 
-export default Home
+const Home = () => {
+  const data = {
+    name: "thapa store",
+  };
+
+  return (
+    <>
+      <HeroSection myData={data} />
+      {/* <FeatureProduct /> */}
+      <Services />
+      <Trusted />
+    </>
+  );
+};
+
+export default Home;
